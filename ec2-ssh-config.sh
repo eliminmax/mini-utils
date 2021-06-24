133061cff58cf4230ae75a3d47ecfcaa3b62a522
Eli Array Minkoff <e.array.minkoff@pm.me>
2021-06-24 10:53:41 -0400

    Fix typo, and remove temporary file

--- ec2-ssh-config.sh
+++ ec2-ssh-config.sh
@@ -5,7 +5,8 @@ mkdir -p ~/.ssh/aws
 
 wget -O /tmp/aws-ip-ranges.json https://ip-ranges.amazonaws.com/ip-ranges.json
 jq -r '.prefixes[] | select(.service=="EC2") | .ip_prefix' </tmp/aws-ip-ranges.json > ~/.ssh/aws/ec2-cidr.json
-printf "Appendinging the following to ~/.ssh/config:\n"
+rm /tmp/aws-ip-ranges.json
+printf "Appending the following to ~/.ssh/config:\n"
 printf "Match exec \"grepcidr -f ~/.ssh/aws/ec2-cidr.json <(echo %%h) &>/dev/null\"\n\tIdentityFile %s\n" \
 	$identityfile | \
 	tee -a ~/.ssh/config
