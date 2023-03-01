a1d0fb7a81be1196b3f3642ecab4a77565b5ffc8
Eli Array Minkoff <eli@earrayminkoff.tech>
2023-02-28 22:35:19 -0500

    Added setup-sandbox.sh

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # mini-utils
 A repository for simple command-line utilites that I've made for myself, but am happy to share with the world.
 
-This is what each utility in the repository does:
+The quality of the scripts and documentation varies. If you don't find something useful, don't use it.
 
+This is what each utility in the repository does:
 
 * `ec2-ssh-config.sh`: a tool to add a setting to the ssh config file for Amazon's EC2 IP addresses, so that it will automatically use an ec2-specific identity file for any of them. Uses **wget** to download a json file from Amazon with a list of their ip subranges, **jq** to parse out the subnets used for ect, and creates an entry in ~/.ssh/config
   * depends: **grepcidr**, **jq**, **wget**
@@ -10,3 +11,6 @@ This is what each utility in the repository does:
   * depends: **python** *(>=3.5)*, **qemu-system-x86_64**, **qemu-kvm** *optional, can use without kvm with lowered performance*, **sudo**
 > Note: QEMU requires root permissions to have write access to the usb drive, and will not boot it without that. I would encourage anyone who wants to use this script to read it over before running it, given that it creates a subprocess with root-level permissions using sudo.
 * `remove-color-escapes`, `show-color-escapes`: pure python tools to remove or show color/text formatting ANSI escape seqences (specifically those that match the regular expression `'\x1b\\[[0-9;]*m'`).
+* `setup-sandbox.sh`: creates a simple sandboxed X11 environment to run a command in.
+  * usage: `setup-sandbox.sh /path/to/sandbox/homedir command-to-run command-args*`
+  * depends: **Xephyr** *(an optional part of Xorg)*, **Bubblewrap** *(a sandboxing tool)*, and **awesome** *(a lightweight window manager)*
---
+++ setup-sandbox.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+PS4='[38;5;195msetup-sandbox.sh »[m'
+set -x
+
+# Usage: setup-sandbox.sh SANDBOX_DIR EXEC_NAME ARGS
+# When used, setup-sandbox.sh does the following:
+# * Creates an isolated X11 environment using Xephyr, and launches the awesome window manager within it.
+# * Runs SANDBOX_DIR/EXEC_NAME ARGS in a simple bubblewrap container, which remounts SANDBOX_DIR over your HOME.
+# Useful if you don't want EXEC_NAME from accessing your actual home. It makes no effort to cut off access to anything outside of your HOME, so it's not the most robust or bulletproof, just useful in a pinch.
+
+sandbox_dir="$1"
+shift
+
+# ensure that Xephyr and Awesome start from the user's home directory
+cd "$HOME" || exit 200
+
+# Get an appropriate number for the Xephyr DISPLAY value
+
+while [ -f /tmp/.get-sandbox-num ]; do
+    echo Waiting for lock held by process "$(cat /tmp/.get-sandbox-num)"
+    sleep 5
+done
+
+# avoid race conditions
+printf '%s' "$$" > /tmp/.get-sandbox-num
+chmod 644 /tmp/.get-sandbox-num
+
+client_display=0
+while [ -S "/tmp/.X11-unix/X$client_display" ]; do
+    client_display="$((client_display+1))"
+done
+
+rm /tmp/.get-sandbox-num
+
+Xephyr -fullscreen ":$client_display" &
+xephyr_pid="$?"
+
+# hang until Xephyr is running - this avoids a race condition with awesome
+until xdpyinfo -display ":$client_display" >/dev/null 2>&1; do true ; done
+
+DISPLAY=":$client_display" awesome &
+awesome_pid="$?"
+
+timestamp="$(date +%s)"
+bus_dir="/run/user/$(id -u)/bus"
+
+cd "$sandbox_dir" || exit 200
+# shellcheck disable=SC2086
+bwrap  \
+    --new-session \
+    --bind /proc /proc \
+    --ro-bind /usr /usr \
+    --ro-bind /var /var \
+    --ro-bind /etc /etc \
+    --dev-bind /dev /dev \
+    --symlink /usr/bin /bin \
+    --symlink /usr/sbin /sbin \
+    --symlink /usr/lib /lib \
+    --symlink /usr/libx32 /libx32 \
+    --symlink /usr/lib32 /lib32 \
+    --symlink /usr/lib64 /lib64 \
+    --setenv DISPLAY ":$client_display" \
+    --bind "$sandbox_dir" "$HOME" \
+    --tmpfs /tmp \
+    --dir /tmp/.X11-unix \
+    --bind "/tmp/.X11-unix/X$client_display" "/tmp/.X11-unix/X$client_display" \
+    --tmpfs /run \
+    --ro-bind "$bus_dir" "$bus_dir" \
+    $SANDBOX_FLAGS -- "$@"
+
+kill "$awesome_pid"
+kill "$xephyr_pid"
